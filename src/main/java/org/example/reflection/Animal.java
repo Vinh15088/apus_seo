a37bc87ec5b89f33fe411c0eb1abfb550afe0b84
Vinh15088 <vinhzz1508@gmail.com>
2025-03-31 14:49:21 +0700

    example reflection

---
+++ src/main/java/org/example/reflection/Animal.java
@@ -0,0 +1,10 @@
+package org.example.reflection;
+
+public abstract class Animal {
+
+    public String getLocation() {
+        return "hehe";
+    }
+
+    public abstract int getNumberOfLegs();
+}
